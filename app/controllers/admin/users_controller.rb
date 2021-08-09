998ab3bafa582b0c491200eb5c26265685226d4f
katsumi-watanabe <ktm981124@gmail.com>
2021-08-09 15:12:24 +0000

    admin/user画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,11 +4,25 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(admin_user_params)
+      redirect_to user_detail_path(@user)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def admin_user_params
+    params.require(:user).permit(:name, :email, :is_deleted)
   end
 end
