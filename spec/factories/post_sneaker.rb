3d35a0a69ce27ec8b655917f803c732f77083a9a
katsumi-watanabe <ktm981124@gmail.com>
2021-09-07 13:39:06 +0000

    rspec

---
+++ spec/factories/post_sneaker.rb
@@ -0,0 +1,7 @@
+# FactoryBot.define do
+#   factory :post_sneaker do
+#     association :user
+#     name { Faker::Lorem.characters(number:30) }
+#     caption { Faker::Lorem.characters(number:90) }
+#   end
+# end
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# FactoryBot.define do
+#   factory :user do
+#     name { Faker::Lorem.characters(number: 10) }
+#     email { Faker::Internet.email }
+#     introduction { Faker::Lorem.characters(number: 50) }
+#     join_year { 'user' }
+#     department_id { 1 }
+#     password { 'userss' }
+#     password_confirmation { 'userss' }
+#   end
+# end
